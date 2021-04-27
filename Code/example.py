50e2d9d2c6dc24ca8881290fc96cd99ffae54911
David Papworth <dpapworth@academytrainee.com>
2021-04-27 14:23:51 +0000

    complete task

--- Code/example.py
+++ Code/example.py
@@ -14,4 +14,9 @@
 	# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
-	return False
+	if list(input)[-1].lower() == "y" and list(input)[-2].lower() == "p":
+		return True
+	else:
+		return False
+
+print(endsPy("tpy"))
